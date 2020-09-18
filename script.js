32833ee3c764c575b2f31f8f438e2d8a51746dd8
Myles Burt <myles_burt@rocketmail.com>
2020-09-18 20:35:14 +0930

    Real time colour changing boxes for hours created.

--- script.js
+++ script.js
@@ -39,18 +39,76 @@ function renderTime () {
         myClock.textContent = "" +dayArray[day]+ " " +dayM+ " " +monthArray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s;
         myClock.innerText = "" +dayArray[day]+ " " +dayM+ " " +monthArray[month]+ " " +year+ " | " +h+ ":" +m+ ":" +s;
 
+        //Past, Present, Future loop.        
+        for(var row = 0; row <= 23; row++) {
+            var currentRow = $("#row" + row);
+            currentRow.removeClass(["past","present","future"]);
+            if (row < h)
+                currentRow.addClass("past");
+            else if (row > h)
+                currentRow.addClass("future");
+            else if (row === h)
+                currentRow.addClass("present");       
+        }
+
+
         setTimeout("renderTime()", 1000);
+
 }
 renderTime();
 
 //Day-month array
 
+// var dateText = "";
+
+
 var dayMonthArray = [
-    "01",
-    "02/09",
-    "03/09",
-    "04/09",
+    "00","01","02","03","04","05","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"
 ];
-document.getElementById("date").innerHTML = dayMonthArray[0] + "/09";
+
+/*
+dayMonthArray.forEach(dateArray);
+document.getElementById("date").innerHTML = dateText;
+
+
+// for (var dateNum = 0; dateNum < dayMonthArray.length; dateNum++)
+
+
+function dateArray(value, index, array) {
+    dateText = dayMonthArray[0] + ":00";
+}
+*/
+
+
+
+
+
+
+/*
+var mulitRow = $("#timeblock");
+
+for (var i = 0,i = dayMonthArray.length; i++) {
+    var newRow = $("<div class="row"><div class="col-2">
+      <p id="date"></p>
+    </div>
+    <div class="col">
+      <input>
+    </div>
+    <div class="col-2">
+      <button type="button" class="btn btn-outline-primary">Save</button>
+    </div>
+  </div> ")
+}
+*/
+
+//document.getElementById("date").innerHTML = dayMonthArray[0] + "/09";
+
+
+//repeating rows
+
+/*
+    for(i = 0;i<29;i++)
+    document.getElementById("timeblock").innerHTML;
+ */
 
  
